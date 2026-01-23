a8db20e88e2e0008fab101c07eb5aee98f1b7e11
nhathuy1945 <nhathuy190405@gmail.com>
2026-01-23 20:16:25 +0700

    Cập nhật ChatFrame – chuẩn hóa import và chốt giao diện chính phía client

--- src/client/gui/ChatFrame.java
+++ src/client/gui/ChatFrame.java
@@ -1,7 +1,21 @@
 package client.gui;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 public class ChatFrame extends JFrame {
     public JTextArea chatArea;
@@ -18,21 +32,19 @@ public class ChatFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         
-        // Chat area
+         
         chatArea = new JTextArea();
         chatArea.setEditable(false);
         chatArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
         JScrollPane chatScroll = new JScrollPane(chatArea);
-        
-        // User list
+         
         userListModel = new DefaultListModel<>();
         userList = new JList<>(userListModel);
         userList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         JScrollPane userScroll = new JScrollPane(userList);
         userScroll.setPreferredSize(new Dimension(150, 0));
         userScroll.setBorder(BorderFactory.createTitledBorder("Online Users"));
-        
-        // Input panel
+         
         JPanel inputPanel = new JPanel(new BorderLayout(5, 5));
         inputPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         
@@ -46,12 +58,10 @@ public class ChatFrame extends JFrame {
         
         inputPanel.add(inputField, BorderLayout.CENTER);
         inputPanel.add(buttonPanel, BorderLayout.EAST);
-        
-        // Status bar
+         
         statusLabel = new JLabel("Connected");
         statusLabel.setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 5));
-        
-        // Layout
+         
         add(chatScroll, BorderLayout.CENTER);
         add(userScroll, BorderLayout.EAST);
         add(inputPanel, BorderLayout.SOUTH);
